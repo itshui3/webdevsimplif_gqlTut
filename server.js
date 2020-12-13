3f8781ddc1574f79e04f700a3c4b923bf4821006
Hui <jeffrey.x.hui@gmail.com>
2020-12-13 04:58:45 -0800

    adds single book query field

--- server.js
+++ server.js
@@ -1,6 +1,109 @@
 
+const _ = require('lodash')
+
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+const {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLList,
+    GraphQLID,
+    GraphQLNonNull,
+} = require('graphql')
 const app = express()
+
+const authors = [
+    { id: 1, name: 'J. K. Rowling' },
+    { id: 2, name: 'J. R. R. Tolkien' },
+    { id: 3, name: 'Brent Weeks' }
+]
+
+const books = [
+    { id: 1, name: 'Harry Potter and the Chamber of Secrets', authorId: 1 },
+    { id: 2, name: 'Harry Potter and the Prisoner of Azkaban', authorId: 1 },
+    { id: 3, name: 'Harry Potter and the Goblet of Fire' , authorId: 1 },
+    { id: 4, name: 'The Fellowship of the Ring', authorId: 2 },
+    { id: 5, name: 'The Two Towers', authorId: 2 },
+    { id: 6, name: 'The Return of the King', authorId: 2 },
+    { id: 7, name: 'The Way of Shadows', authorId: 3 },
+    { id: 8, name: 'Beyond the Shadows', authorId: 3 }
+]
+
+let BookType = new GraphQLObjectType({
+    name: 'Book', 
+    description: 'This represents a book written by an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        authorId: { type: GraphQLNonNull(GraphQLID) },
+        author: { 
+            type: AuthorType,
+            resolve: (parent, args) => {
+                return authors.find((author) => {
+                    return author.id === parent.authorId
+                })
+            }
+        }
+    })
+})
+
+let AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    description: 'This represents an author',
+    fields: () => ({
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (parent, args) => {
+                return books.filter((book) => {
+                    return book.authorId === parent.id
+                })
+            }
+        }
+
+    })
+})
+
+// query is always the root of the request
+// fields off query are the individual datatypes being served
+let query = new GraphQLObjectType({
+    name: 'Query',
+    fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A single book',
+            args: { id: { type: GraphQLID } },
+            resolve: (parent, args) => books.find(book => {
+                console.log(book.id, args.id)
+                return book.id === args.id
+            })
+        },
+
+        books: {
+            type: new GraphQLList(BookType),
+            description: 'A list of books',
+            resolve: () => books
+        },
+
+        authors: {
+            type: new GraphQLList(AuthorType),
+            description: 'A list of authors',
+            resolve: () => authors
+        }
+
+    })
+})
+
+const schema = new GraphQLSchema({
+    query: query
+})
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
 
 app.listen(5000, () => {
     console.log('listening on port 5000')
